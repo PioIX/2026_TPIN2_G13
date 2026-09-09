63dd9cc047d9f3280ae76ab62a6bd2f992e37bb2
Joseg577 <jgroppa@pioix.edu.ar>
2026-09-09 08:40:29 -0300

    creacion tablas

---
+++ docs/script.sql
@@ -0,0 +1,42 @@
+-- 
+CREATE TABLE UsuariosChat (
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    imagen VARCHAR(255)
+);
+
+CREATE TABLE Chats (
+    id_chat INT AUTO_INCREMENT PRIMARY KEY,
+    fecha_creacion DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE ChatUsuarios (
+    id_chat INT,
+    id_usuario INT,
+
+    PRIMARY KEY (id_chat, id_usuario),
+
+    FOREIGN KEY (id_chat) REFERENCES Chats(id_chat),
+    FOREIGN KEY (id_usuario) REFERENCES UsuariosChat(id_usuario)
+);
+
+CREATE TABLE Mensajes (
+    id_mensaje INT AUTO_INCREMENT PRIMARY KEY,
+    id_chat INT NOT NULL,
+    id_usuario INT NOT NULL,
+    contenido TEXT NOT NULL,
+    fecha_hora DATETIME DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (id_chat) REFERENCES Chats(id_chat),
+    FOREIGN KEY (id_usuario) REFERENCES UsuariosChat(id_usuario)
+);
+
+select *
+FROM UsuariosChat;
+
+INSERT INTO UsuariosChat (nombre, email, password, imagen)
+VALUES
+('Juanma', 'juanma@gmail.com', '123456', 'juanma.jpg'),
+('Tini', 'tini@gmail.com', '123456', 'tini.jpg');
